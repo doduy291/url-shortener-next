131fe6bffc3a713a2c5b80b0a58780af14037c66
Dayui <doduy291@gmail.com>
2022-10-16 03:51:18 +0700

    feat: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Home.module.scss";
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>404 Page</h1>
+      </main>
+    </div>
+  );
+};
+
+export default NotFound;
