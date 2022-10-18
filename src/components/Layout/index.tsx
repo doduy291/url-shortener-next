84e0f0d91512f9479c45cc34a35a9d123ffa1a08
Dayui <doduy291@gmail.com>
2022-10-18 23:51:25 +0700

    build: background image

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
+import Image from "next/image";
 import Header from "../Header";
-
+import bgImg from "../../../public/background.svg";
 interface Props {
   children: React.ReactNode;
 }
@@ -7,6 +8,16 @@ interface Props {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="layout">
+      <Image
+        src={bgImg}
+        alt="backgroundImg"
+        quality={100}
+        layout="fill"
+        objectFit="cover"
+        style={{
+          zIndex: -1,
+        }}
+      />
       <Header />
       {children}
     </div>
