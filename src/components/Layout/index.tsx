b16d4ad2ec3a7f62d379aed4898fdf99b7eda890
Dayui <doduy291@gmail.com>
2022-10-24 05:08:22 +0700

    refactor: remove Image background in Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Header from "../Header";
-import bgImg from "../../../public/background.svg";
 interface Props {
   children: React.ReactNode;
 }
@@ -8,16 +6,6 @@ interface Props {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="layout">
-      <Image
-        src={bgImg}
-        alt="backgroundImg"
-        quality={100}
-        layout="fill"
-        objectFit="cover"
-        style={{
-          zIndex: -1,
-        }}
-      />
       <Header />
       {children}
     </div>
