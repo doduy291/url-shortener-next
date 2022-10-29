dd27d80d4b4a1941823bb013ade07e80c88fb6df
Dayui <doduy291@gmail.com>
2022-10-30 04:40:40 +0700

    build: setup global PrismaClient config for NextJS

--- src/utils/prisma-client/index.ts
+++ src/utils/prisma-client/index.ts
@@ -1,10 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
-});
+/* Notice: prevent hot reloading from creating new instances of PrismaClient (for NextJS framework and only development mode) */
+// https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export { prisma };
