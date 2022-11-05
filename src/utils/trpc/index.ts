c8fd1ee83fc389de0811b6401d3984fc9fa63867
Dayui <doduy291@gmail.com>
2022-11-05 17:51:07 +0700

    refactor: restructure base url

--- src/utils/trpc/index.ts
+++ src/utils/trpc/index.ts
@@ -4,16 +4,7 @@ import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { GetInferenceHelpers } from "@trpc/server";
 import type { AppRouter } from "../../server/trpc/routers/_app";
-
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") {
-    return ""; // browser should use relative url
-  }
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  }
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+import { baseUrl } from "../helpers";
 
 export const trpc = createTRPCNext<AppRouter>({
   config() {
@@ -26,7 +17,7 @@ export const trpc = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `${baseUrl()}/api/trpc`,
         }),
       ],
     };
