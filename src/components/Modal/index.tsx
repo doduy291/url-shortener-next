068d312e8328578a02e07e2d036b9a6b5903e7d0
Dayui <doduy291@gmail.com>
2022-11-09 01:15:34 +0700

    build: complement button close for modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -6,7 +6,19 @@ import { ChildrenProps } from "~/types/common";
 const Modal: React.FC<ChildrenProps> = ({ children }) => {
   return ReactDOM.createPortal(
     <div className="modal">
-      <div className={styles.container}>{children}</div>
+      <div className={styles.container}>
+        {children}
+        <div className={styles.closeWrapper}>
+          <svg
+            className={styles.closeIcon}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+          >
+            <path d="M6.414 5A1 1 0 1 0 5 6.414L10.586 12 5 17.586A1 1 0 1 0 6.414 19L12 13.414 17.586 19A1 1 0 1 0 19 17.586L13.414 12 19 6.414A1 1 0 1 0 17.586 5L12 10.586 6.414 5Z"></path>
+          </svg>
+        </div>
+      </div>
       <div className={styles.overlay}></div>
     </div>,
     document.querySelector("body") as HTMLBodyElement,
