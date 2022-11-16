4dbf2129d3773bee4b33125629a1c8cb875d88c1
Dayui <doduy291@gmail.com>
2022-11-16 10:05:44 +0700

    Revert "fix: wrong url still passes"
    This reverts commit 42e43d82f715d0605dc5c2f53b1b1e6aeb51663f.

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -3,7 +3,4 @@ const alphabetStr =
 
 const httpRegex: RegExp = /(^http(s)?:\/\/).*/;
 
-const urlRegex: RegExp =
-  /(http(s)?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-
-export { httpRegex, alphabetStr, urlRegex };
+export { httpRegex, alphabetStr };
